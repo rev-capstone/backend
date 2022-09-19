2d58cfc8385838fd65fc0db50f2e10ae6eec30b8
Raf Treadway <rafael401@revature.net>
2022-09-19 17:19:16 -0400

    added ProductController deleteProduct test

--- src/test/java/com/revature/controllers/ProductControllerTest.java
+++ src/test/java/com/revature/controllers/ProductControllerTest.java
@@ -1,17 +1,30 @@
 package com.revature.controllers;
 
+import java.util.Optional;
+
 import javax.servlet.http.HttpSession;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import com.revature.models.Product;
+import com.revature.services.ProductService;
+
 public class ProductControllerTest {
 
     HttpSession session = Mockito.mock(HttpSession.class);
-    
+    ProductService productService = Mockito.mock(ProductService.class);
 
     @Test
     void testDeleteProduct() {
+        Integer productId = 1;
+        Product product = new Product(1, 15, 40.00, "Special coat", "coat.png", "Special Coat v2");
+
+        Mockito.when(productService.findById(productId)).thenReturn(Optional.of(product));
+
+        productService.delete(productId);
+
+        Mockito.verify(productService, Mockito.times(1)).delete(productId);
         
     }
 
@@ -22,7 +35,9 @@ public class ProductControllerTest {
 
     @Test
     void testGetProductById() {
+        Integer productId = 1;
 
+        
     }
 
     @Test
