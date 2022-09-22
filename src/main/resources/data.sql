576991a871ccdbd3108f7f17c91cb05fa5140460
Raf Treadway <rafael401@revature.net>
2022-09-22 16:14:37 -0400

    new items update on H2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,42 +1,66 @@
 INSERT INTO product (id, quantity, price, description, image, name) VALUES (
     1,
-    10,
-    20.00,
-    'A nice pair of headphones',
-    'https://i.insider.com/54eb437f6bb3f7697f85da71?width=1000&format=jpeg&auto=webp',
-    'Headphones'
+    40,
+    80.00,
+    'Classic 100% organic cotton Revature branded black sweatshirt',
+    'https://s3bucketrt34.s3.amazonaws.com/revsweatshirt.png',
+    'Revature Sweatshirt'
 ),
 (
     2,
-    5,
-    45.00,
-    'A nice TeeShirt',
-    'https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png',
-    'TeeShirt'
+    35,
+    35.00,
+    'If you are going to code all night, you might as well do it in comfort while maintaining that prerequisite sense of humor!',
+    'https://s3bucketrt34.s3.amazonaws.com/coder_jammies-rev.png',
+    'Pajamas'
 ),
 (
     3,
-    20,
-    2.50,
-    'A reusable shopping bag',
-    'https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png',
-    'Shopping Bag'
+    12,
+    75.00,
+    'work on your coding in comfort and efficiency with this beautiful keyboard.  It has a padded wrist rest and can be programmed with macros to supercharge your coding experience!',
+    'https://s3bucketrt34.s3.amazonaws.com/keyboard-rev.png',
+    'Razer Keyboard'
 ),
 (
     4,
-    20,
-    10.00,
-    'A fancy cap for a fancy person',
-    'https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png',
-    'Baseball Cap'
+    35,
+    80.00,
+    'Classic 100% organic cotton HTML5 branded navy blue sweatshirt',
+    'https://s3bucketrt34.s3.amazonaws.com/revsweatshirt2.png',
+    'HTML5 Sweatshirt'
 ),
 (
     5,
-    3,
+    20,
+    30.00,
+    'Love React?  Show everyone with this stylish T!',
+    'https://s3bucketrt34.s3.amazonaws.com/react_tshirt-rev.png',
+    'React T-Shirt'
+),
+(
+    6,
+    35,
     80.00,
-    'A nice coat',
-    'https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png',
-    'Coat'
+    'Classic 100% organic cotton React branded sweatshirt',
+    'https://s3bucketrt34.s3.amazonaws.com/revsweatshirt3.png',
+    'React Sweatshirt'
+),
+(
+    7,
+    40,
+    30.00,
+    'Programming at the local cafe?  Have a cup of java while showing off your love for Java with this cool T',
+    'https://s3bucketrt34.s3.amazonaws.com/spring_tshirt-rev.png',
+    'Spring Boot T-Shirt'
+), 
+(
+    8,
+    25,
+    20.00,
+    'Code for days knowing your wrist tunnels are protected with these supportive gloves.',
+    'https://s3bucketrt34.s3.amazonaws.com/carpalglove-rev.png',
+    'Carpal Gloves'
 );
 
 INSERT INTO users (id, email, password, first_name, last_name) VALUES (
