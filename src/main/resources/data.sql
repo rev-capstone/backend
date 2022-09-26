fbc03451d166db66fec036cda275cc1d759bf223
Raf Treadway <rafael401@revature.net>
2022-09-26 14:45:13 -0400

    adding products to data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,7 +17,7 @@ INSERT INTO product (id, quantity, price, description, image, name) VALUES (
 (
     3,
     12,
-    75.00,
+    175.00,
     'work on your coding in comfort and efficiency with this beautiful keyboard.  It has a padded wrist rest and can be programmed with macros to supercharge your coding experience!',
     'https://s3bucketrt34.s3.amazonaws.com/keyboard-rev.png',
     'Razer Keyboard'
@@ -61,6 +61,134 @@ INSERT INTO product (id, quantity, price, description, image, name) VALUES (
     'Code for days knowing your wrist tunnels are protected with these supportive gloves.',
     'https://s3bucketrt34.s3.amazonaws.com/carpalglove-rev.png',
     'Carpal Gloves'
+),
+(
+    9,
+    50,
+    80.00,
+    'LED backlit keyboard with classic hard clicking keys',
+    'https://s3bucketrt34.s3.amazonaws.com/715XLKbQnFL._AC_SX679_.png',
+    'LED Classic Keyboard'
+),
+(
+    10,
+    30,
+    150.00,
+    'These are great for when you refuse to listen to people talking about React',
+    'https://s3bucketrt34.s3.amazonaws.com/71o8Q5XJS5L._AC_SL1500_.png',
+    'Angular Headphones'
+),
+(
+    11,
+    50,
+    95.00,
+    'Headphones branded by one of the most iconic games of all time',
+    'https://s3bucketrt34.s3.amazonaws.com/71woCid-GQL._AC_SX679_.png',
+    'Doom Headphones'
+),
+(
+    12,
+    70,
+    45.00,
+    'Comfortable contoured mouse that happened to fall off of an Amazon delivery truck',
+    'https://s3bucketrt34.s3.amazonaws.com/AmazonMouse1.png',
+    'Amazon Basics Mouse'
+),
+(
+    13,
+    100,
+    87.00,
+    'Keep those coding fingers warm and ready Revanauts!',
+    'https://s3bucketrt34.s3.amazonaws.com/handwarmer.png',
+    'Revature Handwarmer'
+),
+(
+    14,
+    50,
+    350.00,
+    'You cannot go wrong with a solid RAM upgrade',
+    'https://s3bucketrt34.s3.amazonaws.com/Ram_RipJaws.png',
+    'RipJaws DDR4 RAM Modules'
+),
+(
+    15,
+    40,
+    170.00,
+    'Classic styled mouse with LED lighting and extra side buttons by Razor',
+    'https://s3bucketrt34.s3.amazonaws.com/RazorMouse.png',
+    'Razor Mouse'
+),
+(
+    16,
+    15,
+    250.00,
+    'If you love LEDs, this is the keyboard for you',
+    'https://s3bucketrt34.s3.amazonaws.com/RGB_Keyboard.png',
+    'LED Rounded-key Keyboard'
+),
+(
+    17,
+    6,
+    450.00,
+    'The T is for terrific!',
+    'https://s3bucketrt34.s3.amazonaws.com/T_Mouse.png',
+    'T Mouse'
+),
+(
+    18,
+    35,
+    100.00,
+    'Limited edition Revature Alexandra mug',
+    'https://s3bucketrt34.s3.amazonaws.com/alexandra.png',
+    'Alexandra Mug'
+),
+(
+    19,
+    35,
+    100.00,
+    'Limited edition Revature Ben mug',
+    'https://s3bucketrt34.s3.amazonaws.com/ben.png',
+    'Ben Mug'
+),
+(
+    20,
+    35,
+    100.00,
+    'Limited edition Revature Jonathan humor mug',
+    'https://s3bucketrt34.s3.amazonaws.com/johnathan.png',
+    'Jonathan Humor Mug'
+),
+(
+    21,
+    35,
+    100.00,
+    'Limited edition Revature Jonathan mug',
+    'https://s3bucketrt34.s3.amazonaws.com/johnathan2.png',
+    'Jonathan Mug'
+),
+(
+    22,
+    35,
+    100.00,
+    'Limited edition Revature Kevin mug',
+    'https://s3bucketrt34.s3.amazonaws.com/kevin.png',
+    'Kevin Mug'
+),
+(
+    23,
+    35,
+    100.00,
+    'Limited edition Revature Kevin humor mug',
+    'https://s3bucketrt34.s3.amazonaws.com/kevin2.png',
+    'Kevin Humor Mug'
+),
+(
+    24,
+    35,
+    100.00,
+    'Limited edition Revature Stephen mug',
+    'https://s3bucketrt34.s3.amazonaws.com/stephen.png',
+    'Stephen Mug'
 );
 
 INSERT INTO users (id, email, password, first_name, last_name) VALUES (
