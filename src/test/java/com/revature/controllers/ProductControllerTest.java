4d0a987c9947c8097e35bb1b7cf9d02027de391d
Raf Treadway <rafael401@revature.net>
2022-09-27 18:48:57 -0400

    added purchase full success test to ProductControllerTest, continuing...

--- src/test/java/com/revature/controllers/ProductControllerTest.java
+++ src/test/java/com/revature/controllers/ProductControllerTest.java
@@ -1,5 +1,7 @@
 package com.revature.controllers;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -27,6 +29,20 @@ public class ProductControllerTest {
     Product p1 = new Product(1, 15, 40.00, "Special coat", "coat.png", "Special Coat v2");
     Product p2 = new Product(2, 45, 13.00, "Magic Socks", "socks.png", "Magic Socks");
     Product p3 = new Product(3, 4, 370.00, "Iron T-Shirt", "tshirt.png", "Iron plate T-Shirt");
+    List<Product> productList = new ArrayList<Product>(){
+        {
+            add(p1);
+            add(p2);
+            add(p3);
+        }
+    };
+    ArrayList<ProductInfo> metadata = new ArrayList<ProductInfo>(){
+        {
+            add(new ProductInfo(1,3));
+            add(new ProductInfo(2,15));
+            add(new ProductInfo(3,1));
+        }
+    };
 
     @Test
     void testDeleteProductSuccess() {
@@ -35,7 +51,7 @@ public class ProductControllerTest {
 
         Mockito.when(productService.findById(productId)).thenReturn(Optional.of(product));
 
-        Assertions.assertEquals(ResponseEntity.ok(product), productController.deleteProduct(productId));
+        assertEquals(ResponseEntity.ok(product), productController.deleteProduct(productId));
 
         Mockito.verify(productService, Mockito.times(1)).delete(productId);
     }
@@ -46,7 +62,7 @@ public class ProductControllerTest {
 
         Mockito.when(productService.findById(productId)).thenReturn(Optional.empty());
 
-        Assertions.assertEquals(ResponseEntity.notFound().build(), productController.deleteProduct(productId));
+        assertEquals(ResponseEntity.notFound().build(), productController.deleteProduct(productId));
     }
 
     @Test
@@ -54,7 +70,7 @@ public class ProductControllerTest {
         List<Product> product_list_response = new ArrayList<Product>();
         Mockito.when(productService.findAll()).thenReturn(product_list_response);
 
-        Assertions.assertEquals(ResponseEntity.ok(product_list_response), productController.getInventory());
+        assertEquals(ResponseEntity.ok(product_list_response), productController.getInventory());
     }
 
     @Test
@@ -63,38 +79,30 @@ public class ProductControllerTest {
         Product product = p1;
         Mockito.when(productService.findById(productId)).thenReturn(Optional.of(product));
 
-        Assertions.assertEquals(ResponseEntity.ok(product), productController.getProductById(productId));
+        assertEquals(ResponseEntity.ok(product), productController.getProductById(productId));
     }
 
     @Test
     void testGetProductByIdFail(){
         Integer productId = 1;
         Mockito.when(productService.findById(productId)).thenReturn(Optional.empty());
-        Assertions.assertEquals(ResponseEntity.notFound().build(), productController.getProductById(productId));
+        assertEquals(ResponseEntity.notFound().build(), productController.getProductById(productId));
     }
 
-
     @Test
-    void testPurchaseSuccessful() {
-        ArrayList<Product> productList = new ArrayList<Product>(){
-            {
-                add(p1);
-                add(p2);
-                add(p3);
-            }
-        };
-        List<ProductInfo> metadata = new ArrayList<ProductInfo>(){
-            {
-                add(new ProductInfo(1,3));
-                add(new ProductInfo(2,15));
-                add(new ProductInfo(3,1));
-            }
-        };
-        // Mockito.when(productList.iterator()).thenReturn(productIterator);
-        // Mockito.when(productIterator.hasNext()).thenReturn(true, true, true);
-        // Mockito.when(productIterator.next()).thenReturn(p1).thenReturn(p2).thenReturn(p3);
-        productService.saveAll(productList, metadata);
-        Mockito.verify(productService, Mockito.times(1)).saveAll(productList, metadata);
+    void testPurchaseProductsFoundAndSaved() {
+        ProductInfo currentMeta;
+        Integer currentId;
+        for(int i = 0; i < metadata.size(); i++){
+            currentMeta = metadata.get(i);
+            currentId = metadata.get(i).getId();
+            Mockito.when(productService.findById(currentId)).thenReturn(Optional.of(productList.get(i)));
+            assertEquals(Optional.of(productList.get(i)), productService.findById(currentId));
+            Optional<Product> p = productService.findById(currentId);
+            assertTrue(p.get().getQuantity() - currentMeta.getQuantity() > 0);
+        }
+        Mockito.when(productService.saveAll(productList, metadata)).thenReturn(productList);
+        assertEquals(productList, productService.saveAll(productList, metadata));  
     }
 
     @Test
@@ -102,7 +110,7 @@ public class ProductControllerTest {
         Product product = new Product(1, 15, 40.00, "Special coat", "coat.png", "Special Coat v2");
         Mockito.when(productService.save(product)).thenReturn(product);
 
-        Assertions.assertEquals(ResponseEntity.ok(product), productController.upsert(product));
+        assertEquals(ResponseEntity.ok(product), productController.upsert(product));
 
         Mockito.verify(productService, Mockito.times(1)).save(product);
     }
