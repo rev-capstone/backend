9c66a4f2acc7d95198ef367b5ca95f597d3ad031
JaDePat <jdp664@humboldt.edu>
2022-09-27 14:30:15 -0700

    working on user service tests

--- src/test/java/com/revature/services/UserServiceTest.java
+++ src/test/java/com/revature/services/UserServiceTest.java
@@ -1,5 +1,7 @@
 package com.revature.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
@@ -8,9 +10,11 @@ import org.mockito.Mockito;
 
 import com.revature.dtos.LoginRequest;
 import com.revature.models.User;
+import com.revature.repositories.UserRepository;
 
 public class UserServiceTest {
-    UserService userService = Mockito.mock(UserService.class);
+    UserRepository userRepository = Mockito.mock(UserRepository.class);
+    UserService userService = new UserService(userRepository);
 
     @Test
     void testFindByCredentialsSuccessful() {
@@ -29,4 +33,18 @@ public class UserServiceTest {
 
         Assertions.assertEquals(Optional.empty(), userService.findByCredentials(loginRequest.getEmail(), loginRequest.getPassword()));
     }
+
+    @Test
+    void testSaveUserSuccessful() {
+        //arrange
+        User expected = new User(1, "j@gmail.com", "password", "firstName", "lastName");
+        Mockito.when(userRepository.save(expected)).thenReturn(expected);
+
+        //act
+        User actual = userService.save(expected);
+
+        //assert
+        assertEquals(expected, actual);
+    }
+
 }
