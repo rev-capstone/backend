438af249635762dc4158676918a8847f519e853d
Mark Maupin <m.w.maupin@gmail.com>
2022-10-04 11:36:34 -0400

    config cors

--- src/main/java/com/revature/config/ApplicationConfig.java
+++ src/main/java/com/revature/config/ApplicationConfig.java
@@ -19,7 +19,7 @@ public class ApplicationConfig {
         return new WebMvcConfigurer() {
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                .allowedOrigins(System.getenv("http://cappyrevaturebucket.s3-website-us-east-1.amazonaws.com"))
+                .allowedOrigins("http://cappyrevaturebucket.s3-website-us-east-1.amazonaws.com")
                 .allowedMethods("*")
                 .allowCredentials(true);
             }
