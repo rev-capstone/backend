e5bcba5b52199472aca384d1474dcf68ee90da7a
Mark Maupin <m.w.maupin@gmail.com>
2022-10-04 11:56:24 -0400

    test method

--- src/main/java/com/revature/controllers/AuthController.java
+++ src/main/java/com/revature/controllers/AuthController.java
@@ -21,6 +21,12 @@ public class AuthController {
         this.authService = authService;
     }
 
+    @GetMapping
+    public User test(){
+
+        return null;
+    }
+
     @PostMapping("/login")
     public ResponseEntity<User> login(@RequestBody LoginRequest loginRequest, HttpSession session) {
         Optional<User> optional = authService.findByCredentials(loginRequest.getEmail(), loginRequest.getPassword());
